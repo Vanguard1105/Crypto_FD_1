0c7ff2dfd2000b768435209cea3c3a9583ecf784
Vanguard1105 <vanguard951105@gmail.com>
2025-03-30 08:04:46 -0700

    fix: lottery page design updated

--- src/pages/Lottery.tsx
+++ src/pages/Lottery.tsx
@@ -21,10 +21,10 @@ const Lottery = () => {
     { id: 12, date: '2025.03.24', startTime: '09:30:00', status: 'upcoming', type: 'predict' },
     { id: 11, date: '2025.03.23', startTime: '11:20:00', status: 'ended', type: 'vote' },
     { id: 10, date: '2025.03.22', startTime: '18:00:00', status: 'ended', type: 'predict' },
-    { id: 13, date: '2025.03.24', startTime: '12:00:00', status: 'upcoming', type: 'vote' },
-    { id: 12, date: '2025.03.24', startTime: '09:30:00', status: 'upcoming', type: 'predict' },
-    { id: 11, date: '2025.03.23', startTime: '11:20:00', status: 'ended', type: 'vote' },
-    { id: 10, date: '2025.03.22', startTime: '18:00:00', status: 'ended', type: 'predict' },
+    { id: 12, date: '2025.03.24', startTime: '12:00:00', status: 'upcoming', type: 'vote' },
+    { id: 13, date: '2025.03.24', startTime: '09:30:00', status: 'upcoming', type: 'predict' },
+    { id: 10, date: '2025.03.23', startTime: '11:20:00', status: 'ended', type: 'vote' },
+    { id: 11, date: '2025.03.22', startTime: '18:00:00', status: 'ended', type: 'predict' },
   ];
 
   const filteredLotteries = lotteries.filter(lottery => lottery.type === selectedType);
@@ -109,10 +109,10 @@ const Lottery = () => {
       </div>
 
       {/* Lottery List */}
-      <div className="px-4 space-y-2 py-2">
-        {filteredLotteries.map((lottery, index) => (
+      <div className="px-4 space-y-1 py-2">
+        {filteredLotteries.map((lottery) => (
           <div
-          key={index}
+          key={lottery.id}
           className={`relative rounded-xl overflow-hidden group cursor-pointer ${
             theme === 'dark' ? 'bg-slate-800' : 'bg-white'
           } shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-md ${
