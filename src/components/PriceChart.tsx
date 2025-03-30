a17090cd589fb780c7670e68c82663940923fa72
Vanguard1105 <vanguard951105@gmail.com>
2025-03-29 18:26:34 -0700

    fix: lottery page design updated

--- src/components/PriceChart.tsx
+++ src/components/PriceChart.tsx
@@ -94,13 +94,20 @@ const PriceChart: React.FC<PriceChartProps> = ({ data, latestPrice, previousPric
       theme === 'dark' ? 'border-slate-700/50' : 'border-blue-300'
     }`}>      
       <div className="h-[180px] w-full">
-        <div className="flex items-center justify-end mr-4 h-5">
+        <div className="flex items-center justify-between mr-10 h-5">
+          <div className='flex flex-row gap-2'>
+            <div>
+              <span className='text-lg text-blue-900 font-semibold'>SOL</span>/
+              <span className='text-lg text-[#811f1a] font-semibold'>USD</span>
+            </div>
+            <p className='text-[#286234'>${latestPrice}</p>
+          </div>
           <div className="flex p-1">
             {timeFilters.map(({ label, value }) => (
               <button
                 key={value}
                 onClick={() => onPeriodChange(value as TimePeriod)}
-                className={`flex items-center justify-center mx-[2px] px-1 rounded text-[10px] w-[25px] font-medium transition-all duration-200 border border-slate-900 ${
+                className={`flex items-center justify-center mx-[2px] px-1 rounded text-[10px] w-[25px] font-medium transition-all duration-200 border border-slate-600 ${
                   period === value
                     ? 'bg-[#5856d6] text-white shadow'
                     : 'text-slate-600'
